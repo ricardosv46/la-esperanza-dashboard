0c1a34b41699f1723f8fa61482f41eb130e1239b
ricardosv46 <ricardosv46@gmail.com>
2022-08-23 18:19:48 -0500

    feat: se agrega validacion por fechas en abonos y eventos

--- src/pages/home/bloqueo/ButacasPage.tsx
+++ src/pages/home/bloqueo/ButacasPage.tsx
@@ -1,11 +1,19 @@
-import { Box, Center, Container, Flex, Heading, Image, Spinner, Text } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import { Box, Button, Center, Container, Flex, Heading, Image, Spinner, Text } from '@chakra-ui/react'
+import moment from 'moment'
+import { Link, useNavigate } from 'react-router-dom'
 import useEventos from '../../../services/useEventos'
 import useFeria from '../../../services/useFeria'
 
 const ButacasPage = () => {
 	const { db: abono, loading: loadingFeria } = useFeria()
 	const { db: eventos, loading: loadingEventos } = useEventos({ estado: 'Activado', feriaId: 1 })
+	const { fechaFinal, fechaInicial, horaFinal, horaInicial } = abono
+
+	const fechaI = `${fechaInicial} ${horaInicial}`
+	const fechaF = `${fechaFinal} ${horaFinal}`
+	const isDisableAbono = moment().isBetween(moment(fechaI), moment(fechaF)) ? false : true
+
+	const router = useNavigate()
 
 	return (
 		<Container maxWidth='1930px' p={'10'}>
@@ -24,10 +32,16 @@ const ButacasPage = () => {
 				color='gray.700'
 				fontWeight='bold'
 				textAlign='center'>
-				<Link to='/bloqueo/abono'>
-					<Box w={64}>
+				<button
+					className='w-64'
+					disabled={isDisableAbono}
+					onClick={() => {
+						router('/bloqueo/abono')
+					}}>
+					<Box filter={isDisableAbono ? 'grayscale(100%)' : ''}>
 						<Box position='relative' rounded='lg' overflow='hidden' w={64} h={36}>
 							<Image
+								filter={isDisableAbono ? 'grayscale(100%)' : ''}
 								src={abono?.imagenPrincipal?.url!}
 								alt={abono?.titulo!}
 								fallback={
@@ -44,11 +58,14 @@ const ButacasPage = () => {
 							/>
 						</Box>
 
-						<Text zIndex={10} color='#F0AC42' _dark={{ color: 'primary.500' }}>
+						<Text
+							zIndex={10}
+							color={isDisableAbono ? 'gray.200' : '#F0AC42'}
+							_dark={{ color: 'primary.500' }}>
 							{abono?.titulo!}
 						</Text>
 					</Box>
-				</Link>
+				</button>
 			</Flex>
 			<Heading as='h2' my={5} fontSize={22}>
 				Eventos
@@ -62,33 +79,48 @@ const ButacasPage = () => {
 				color='gray.700'
 				fontWeight='bold'
 				textAlign='center'>
-				{eventos.map((evento) => (
-					<Link key={evento?.eventoId} to={`/bloqueo/evento/${evento?.slug!}`} state={{ evento }}>
-						<Box w={64}>
-							<Box position='relative' rounded='lg' overflow='hidden' w={64} h={36}>
-								<Image
-									src={evento?.imagenPrincipal?.url!}
-									alt={evento?.titulo!}
-									fallback={
-										<Center>
-											<Spinner colorScheme='primary' size='lg' />
-										</Center>
-									}
-									w='full'
-									h='full'
-									pos='absolute'
-									inset={0}
-									objectFit='cover'
-									zIndex={10}
-								/>
-							</Box>
+				{eventos.map((evento) => {
+					const fechaInicialEvento = `${evento?.fechaInicial} ${evento?.horaInicial}`
+					const fechaFinalEvento = `${evento?.fechaFinal} ${evento?.horaFinal}`
 
-							<Text zIndex={10} color='#F0AC42' _dark={{ color: 'primary.500' }}>
-								{evento?.titulo}
-							</Text>
-						</Box>
-					</Link>
-				))}
+					const isDisabledEvento = moment().isBetween(moment(fechaInicialEvento), moment(fechaFinalEvento))
+						? false
+						: true
+					return (
+						<button
+							className='w-64'
+							disabled={isDisabledEvento}
+							onClick={() => {
+								router(`/bloqueo/evento/${evento?.slug!}`, {
+									state: { evento }
+								})
+							}}>
+							<Box filter={isDisabledEvento ? 'grayscale(100%)' : ''}>
+								<Box position='relative' rounded='lg' overflow='hidden' w={64} h={36}>
+									<Image
+										src={evento?.imagenPrincipal?.url!}
+										alt={evento?.titulo!}
+										fallback={
+											<Center>
+												<Spinner colorScheme='primary' size='lg' />
+											</Center>
+										}
+										w='full'
+										h='full'
+										pos='absolute'
+										inset={0}
+										objectFit='cover'
+										zIndex={10}
+									/>
+								</Box>
+
+								<Text zIndex={10} color='#F0AC42' _dark={{ color: 'primary.500' }}>
+									{evento?.titulo}
+								</Text>
+							</Box>
+						</button>
+					)
+				})}
 			</Flex>
 		</Container>
 	)
